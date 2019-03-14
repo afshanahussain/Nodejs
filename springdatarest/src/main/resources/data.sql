b66fbfc4026de4140dfaa5f31f2011b5259e45b3
afshanahussain <48115835+afshanahussain@users.noreply.github.com>
2019-03-14 00:51:57 -0700

    Delete data.sql

--- springdatarest/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into alien values(1,'Mehar','java');
-insert into alien values(2,'Afshana','js');
-insert into alien values(3,'Hussain','java');
-insert into alien values(4,'Runa', 'java');
-insert into alien values(5,'Salimah','js');
